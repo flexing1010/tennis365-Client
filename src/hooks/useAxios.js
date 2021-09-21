3c41e80f968cf5c012c4f6998c73d9ef7830e285
flexing1010 <kjzon@hotmail.com>
2021-09-22 00:57:19 +0900

    bug fix

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://tennis365-api.herokuapp.com";
 // const useAxios = ({ url, method, body = null, params = null }) => {
 //   const [response, setResponse] = useState(null);
 //   const [errorMessage, setErrorMessage] = useState("");
