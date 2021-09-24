1df0e20c0231aa89c4d342c92c25abc233f95452
flexing1010 <kjzon@hotmail.com>
2021-09-24 21:14:46 +0900

    mobile payment test1.6

--- src/components/MobilePayment/MobilePayment.jsx
+++ src/components/MobilePayment/MobilePayment.jsx
@@ -9,6 +9,7 @@ const MobilePayment = () => {
   let location = useLocation();
   const { search } = location;
   const query = queryString.parse(search);
+  const { merchant_uid } = query;
 
   useEffect(() => {
     console.log(orderData);
@@ -16,9 +17,15 @@ const MobilePayment = () => {
 
   return (
     <div>
-      {orderData && <h1>{orderData.user_id}</h1>}
-      <h1>{orderData.order_id}</h1>}<h1>{orderData.status}</h1>}
-      <h1>{orderData.orderItems[0]}</h1>}<h1>{orderData.amount}</h1>}
+      <h1>{merchant_uid}</h1>
+      {orderData && (
+        <>
+          ( <h1>{orderData.user_id}</h1>
+          <h1>{orderData.order_id}</h1>
+          <h1>{orderData.status}</h1>}<h1>{orderData.orderItems[0]}</h1>
+          <h1>{orderData.amount}</h1>)<h1>{`${merchant_uid}00`}</h1>
+        </>
+      )}
     </div>
   );
 };
